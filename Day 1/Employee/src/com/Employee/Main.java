5c80feb24da3f74ca67e31fcb76f843a21f8f9b2
HarshalPatil11 <73750028+HarshalPatil11@users.noreply.github.com>
2020-10-31 20:53:23 +0530

    Day 2 assignments submitted

---
+++ Day 1/Employee/src/com/Employee/Main.java
@@ -0,0 +1,19 @@
+package com.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        employee e1=new employee();
+        e1.name="The name is Harshal";
+        e1.age=20;
+        e1.city="The city is Sangli";
+        e1.display();
+
+        employee e2=new employee();
+        e2.name="The name is Vishal";
+        e2.age=21;
+        e2.city="The city is Beed";
+        e2.display();
+    }
+}
---
+++ Day 1/Employee/src/com/Employee/employee.java
@@ -0,0 +1,13 @@
+package com.Employee;
+
+public class employee {
+
+    public String name, city;
+    public int age;
+
+    public void display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
